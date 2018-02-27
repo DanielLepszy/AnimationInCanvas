57fdd62408fe8fff0253ed9012ea887f5cd82c9c
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-02-28 00:12:12 +0100

    Add comments and improve clean code

--- functionalityOfAnimations.js
+++ functionalityOfAnimations.js
@@ -7,7 +7,7 @@ canv.height = 864;
 const cw = canv.width
 const ch = canv.height
 const circleSize = 50;
-var amountOfBigCircle = 7;
+var amountOfBigCircle = 3;
 let amountOfCheckingPosition = 0;
 let circleX;
 let circleY;
@@ -18,29 +18,24 @@ var positionYCircle = [];
 table();
 showCircleOnWindow();
 
-
 function table() {
     ctx.fillStyle = "#212320"
     ctx.fillRect(0, 0, cw, ch);
 }
-
 function showCircleOnWindow() {
     getPositionXY();
     blockShowingCirclesOnTheSamePosition();
     allCircle();
 }
-
 function allCircle() {
     bigCircle();
     // mediumCircle();
     // smallCircle();
 }
-
 function randomPositions() {
     circleX = Math.floor(Math.random() * (1536 - 2 * circleSize) + circleSize);
     circleY = Math.floor(Math.random() * (864 - 2 * circleSize) + circleSize);
 }
-
 function getPositionXY() {
     for (var i = 1; i <= amountOfBigCircle; i++) {
         randomPositions()
@@ -48,7 +43,6 @@ function getPositionXY() {
         positionYCircle.push(circleY);
     }
 }
-
 function blockShowingCirclesOnTheSamePosition() {
     for (var i = 0; i <= amountOfBigCircle - 1; i++) {
         for (var j = i + 1; j <= amountOfBigCircle - 1; j++) {
@@ -64,9 +58,10 @@ function blockShowingCirclesOnTheSamePosition() {
         }
     }
 }
-//Size of Circle ---------
+//SIZE OF CIRCLE ---------
 function bigCircle() {
     for (var i = 0; i <= amountOfBigCircle - 1; i++) {
+        //CREAT CIRCLE-----------
         ctx.beginPath();
         ctx.arc(positionXCircle[i], positionYCircle[i], circleSize, 0 * Math.PI, 2 * Math.PI);
         ctx.fillStyle = "blue";
@@ -76,27 +71,24 @@ function bigCircle() {
         ctx.fillStyle = "black";
         ctx.fill();
         ctx.stroke();
-        //Line Circle----------
+        //CREATE LINES-----------
         ctx.beginPath();
         ctx.moveTo(positionXCircle[i], positionYCircle[i]);
         ctx.lineTo(positionXCircle[i + 1], positionYCircle[i + 1]);
         ctx.stroke();
-        // ADDING LAST LINE CIRCLE TO THE FIRST --------
+        // ADDING LAST LINE CIRCLE TO THE FIRST-----------
         ctx.beginPath();
         ctx.moveTo(positionXCircle[amountOfBigCircle - 1], positionYCircle[amountOfBigCircle - 1]);
         ctx.lineTo(positionXCircle[i], positionYCircle[i]);
         ctx.stroke();
     }
-
 }
-
 function lineCircle() {
     ctx.beginPath();
     ctx.moveTo(positionXCircle[i], positionYCircle[i]);
     ctx.lineTo(positionXCircle[i + 1], positionYCircle[i + 1]);
     ctx.stroke();
 }
-
 function mediumCircle() {
     ctx.beginPath();
     ctx.arc(400, 200, circleSize - 10, 0, 2 * Math.PI);
@@ -108,7 +100,6 @@ function mediumCircle() {
     ctx.fill();
     ctx.stroke();
 }
-
 function smallCircle() {
     ctx.beginPath();
     ctx.arc(200, 200, circleSize - 20, 0, 2 * Math.PI);
