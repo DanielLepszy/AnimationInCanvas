4ba071622c72f7979d1a1fe9485d96f01bb464f3
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-03-03 16:27:14 +0100

    Random size of circles

--- functionalityOfAnimations.js
+++ functionalityOfAnimations.js
@@ -1,6 +1,7 @@
 class Circle {
-    constructor(point) {
+    constructor(point, radius) {
         this.point = point;
+        this.radius = radius;
     }
 }
 class CirclePoint {
@@ -16,11 +17,13 @@ var speedY = [];
 canv.width = 1536;
 canv.height = 864;
 
-const cw = canv.width
-const ch = canv.height
+const CANVAS_WIDTH = canv.width
+const CANVAS_HEIGHT = canv.height
 var circles = [];
-const circleSize = 50;
-var amountOfBigCircle = 5;
+const BIG_SIZE = 50;
+const MEDIUM_SIZE = 40;
+const SMALL_SIZE = 25;
+var amountOfBigCircle = 15;
 let amountOfCheckingPosition = 0;
 
 
@@ -29,21 +32,29 @@ showCircleOnWindow();
 
 function table() {
     ctx.fillStyle = "#212320"
-    ctx.fillRect(0, 0, cw, ch);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
 function showCircleOnWindow() {
-    createBigCircles()
+    createCircles()
     randomSpeedCircle()
     setInterval(allCircle, 0.1);
-   //allCircle()
+}
+
+function randomSizeOfCircle() {
+    switch (Math.floor(Math.random() * 3) + 1) {
+        case 1: return SMALL_SIZE;
+        case 2: return MEDIUM_SIZE;
+        case 3: return BIG_SIZE;
+        default: return MEDIUM_SIZE;
+    }
 }
 
 function reflectionCirclesFromWindow() {
-    for (var i = 0; i <= amountOfBigCircle - 1; i++) {
-        if (circles[i].point.x < circleSize || circles[i].point.x >= cw - circleSize) {
+    for (var i = 0; i <= circles.length - 1; i++) {
+        if (circles[i].point.x < circles[i].radius || circles[i].point.x >= CANVAS_WIDTH - circles[i].radius) {
             speedX[i] = -speedX[i];
-        } else if (circles[i].point.y < circleSize || circles[i].point.y >= ch - circleSize) {
+        } else if (circles[i].point.y < circles[i].radius || circles[i].point.y >= CANVAS_HEIGHT - circles[i].radius) {
             speedY[i] = -speedY[i];
         }
     }
@@ -51,18 +62,17 @@ function reflectionCirclesFromWindow() {
 
 function reflectionCirclesFromEachOther() {
     for (var i = 0; i < circles.length; i++) {
-        for (var j = i+1; j < circles.length; j++) {
+        for (var j = i + 1; j < circles.length; j++) {
             var distanceCircles = Math.sqrt(Math.pow(circles[i].point.x - circles[j].point.x, 2) + Math.pow(circles[i].point.y - circles[j].point.y, 2));
-             if (distanceCircles <= 95) {
-            // console.log(speedY[j]);
-            speedY[i] = -speedY[i]; 
-            speedX[i] = -speedX[i];
-            speedY[j] = -speedY[j];
-            speedX[j] = -speedX[j];
-        
+            if (distanceCircles <= circles[i].radius) {
+                speedY[i] = -speedY[i];
+                speedX[i] = -speedX[i];
+                speedY[j] = -speedY[j];
+                speedX[j] = -speedX[j];
+
+            }
         }
     }
-}
 }
 
 
@@ -82,33 +92,35 @@ function randomSpeedCircle() {
     }
 }
 
-function getPoint() {
-    const circleX = Math.floor(Math.random() * (1536 - 2 * circleSize) + circleSize);
-    const circleY = Math.floor(Math.random() * (864 - 2 * circleSize) + circleSize);
+function getPoint(radius) {
+    const circleX = Math.floor(Math.random() * (1536 - 2 * radius) + radius);
+    const circleY = Math.floor(Math.random() * (864 - 2 * radius) + radius);
     const point = new CirclePoint(circleX, circleY)
     return point;
 }
 
-function createBigCircles() {
+function createCircles() {
     for (var i = 0; i <= amountOfBigCircle - 1; i++) {
-        var point = getPoint();
-        while (!isPointCorrect(point)) {
+        const radius = randomSizeOfCircle(); // TU LOSOWAC SIZE! ----------
+        var point = getPoint(radius);
+        while (!isPointCorrect(point, radius)) {
             point = getPoint();
         }
-        const circle = new Circle(point)
+
+        const circle = new Circle(point, radius)
         circles.push(circle)
     }
 }
 
-function checkIfCirclesOverlap(pointA, pointB) {
-    return Math.hypot(pointA.x - pointB.x, pointA.y - pointB.y) <= (circleSize + circleSize);
+function checkIfCirclesOverlap(pointA, pointB, radius) {
+    return Math.hypot(pointA.x - pointB.x, pointA.y - pointB.y) <= (radius + radius);
 }
 
-function isPointCorrect(point) {
+function isPointCorrect(point, radius) {
     var isCorrect = true
     for (let index = 0; index < circles.length; index++) {
         const circle = circles[index];
-        if (checkIfCirclesOverlap(point, circle.point)) {
+        if (checkIfCirclesOverlap(point, circle.point, radius)) {
             isCorrect = false;
             break;
         }
@@ -116,18 +128,17 @@ function isPointCorrect(point) {
     return isCorrect
 }
 
-//SIZE OF CIRCLE --------------------
 function drawBigCircles() {
     for (var i = 0; i <= amountOfBigCircle - 1; i++) {
         //CREAT CIRCLE ----------------------
 
         const circle = circles[i];
         ctx.beginPath();
-        ctx.arc(circle.point.x, circle.point.y, circleSize, 0 * Math.PI, 2 * Math.PI);
+        ctx.arc(circle.point.x, circle.point.y, circle.radius, 0 * Math.PI, 2 * Math.PI);
         ctx.fillStyle = "blue";
         ctx.fill();
         ctx.beginPath();
-        ctx.arc(circle.point.x, circle.point.y, circleSize - 10, 0, 2 * Math.PI);
+        ctx.arc(circle.point.x, circle.point.y, circle.radius - 10, 0, 2 * Math.PI);
         ctx.fillStyle = "black";
         ctx.fill();
         ctx.stroke();
@@ -145,7 +156,7 @@ function lineOfBigCircles() {
                 var opacityOfLine = z / 10;
                 var lengthOfLine = Math.sqrt(Math.pow(circles[i].point.x - circles[j].point.x, 2) + Math.pow(circles[i].point.y - circles[j].point.y, 2));
                 distanceFromCircles += 40;
-                if (lengthOfLine <= (circleSize + distanceFromCircles)) {
+                if (lengthOfLine <= (circles[i].radius + distanceFromCircles)) {
                     ctx.moveTo(circles[j].point.x, circles[j].point.y);
                     ctx.lineTo(circles[i].point.x, circles[i].point.y);
                     ctx.strokeStyle = "rgba(17, 95, 251," + opacityOfLine + ")";
